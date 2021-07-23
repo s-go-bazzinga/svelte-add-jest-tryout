663f8f9f2ed11e52adf4e3affac5e67013ec76b4
Saikat Das <s@gobazzinga.io>
2021-07-23 15:30:22 +0530

    feat: init repo

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { browser } from '$app/env';
+
+	console.log(browser);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Hello world!</h1>
